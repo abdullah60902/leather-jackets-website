01b0561d965ac65407630f4ed3be3d11aa61d9d6
abdullah60902 <butt60902@gmail.com>
2026-03-04 06:26:04 +0500

    Remove test files

--- test-env.js
+++
@@ -1,6 +0,0 @@
-console.log('--- ENV TEST ---');
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
-console.log('EMAIL_HOST:', process.env.EMAIL_HOST);
-console.log('EMAIL_PORT:', process.env.EMAIL_PORT);
-console.log('----------------');
